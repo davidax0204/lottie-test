6e2cb1949275d110fa977afbd2b9bf83ea558a67
davidax0204 <davidax5625@gmail.com>
2024-03-20 11:55:49 +0200

    v6 html and ts removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  mainObj:any={};
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  myOTP:any;
-}
+export class AppComponent {}
