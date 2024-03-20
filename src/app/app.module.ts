1396107255d88598a0a35b49a8e8c7f94da864fa
davidax0204 <davidax5625@gmail.com>
2024-03-20 12:23:25 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { WebOtpDirective } from './web-otp.directive';
-import { WebOtp2Directive } from './web-otp2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     WebOtpDirective,
-    WebOtp2Directive,
   ],
   imports: [
     BrowserModule,
