1e18089f29070024735ce4b5e6d3f318662ec8b6
davidax0204 <davidax5625@gmail.com>
2024-03-20 14:15:17 +0200

    moved to comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  mainObj:any={};
-  constructor() { }
+  constructor() {}
+  input:string=''
+  private abortController = new AbortController();
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    if ('OTPCredential' in window) {
+      window.addEventListener('DOMContentLoaded', () => {
+        this.handleWebOTP();
+      });
+    }
   }
 
-  myOTP:any;
+  private handleWebOTP(): void {
+    const otpRequest = {
+      otp: { transport:['sms'] },
+      signal: this.abortController.signal
+    };
+
+    navigator.credentials.get(otpRequest)
+      .then((otp: any) => {
+        if (otp && otp.code) {
+          this.input = otp.code
+        }
+      });
+  }
 }
